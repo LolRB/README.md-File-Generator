d6ea013bf3f8886dc1ca93aa2a0887d95e01dade
LolRB <rodrigo_luuu@hotmail.com>
2024-08-13 13:42:00 -0600

    Created questions

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+const questions = [
+  {
+    type: "input",
+    message: "What's your Github username?\n",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What's your Github repo?\n",
+    name: "repo",
+  },
+  {
+    type: "input",
+    message: "What's your project's title?\n",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What's your project's introduction?\n",
+    name: "intro",
+  },
+  {
+    type: "input",
+    message: "What're your project's features?\n",
+    name: "features",
+  },
+  {
+    type: "input",
+    message: "Explain how to use your app\n",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "How do you install this app?\n",
+    name: "installation",
+  },
+  {
+    type: "checkbox",
+    message: "What's your project's title?\n",
+    choices: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "SQL",
+      "MongoDB",
+      "Node",
+      "Java",
+      "C#",
+      "Python",
+    ],
+    name: "technologies",
+  },
+];
 
 // Function call to initialize app
 init();
