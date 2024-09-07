0acc29ad306eafa6d29b5c6aebab20515c54894c
LolRB <rodrigo_luuu@hotmail.com>
2024-09-07 15:04:59 -0600

    Polished details and added comments

--- index.js
+++ index.js
@@ -57,19 +57,50 @@ const questions = [
     ],
     name: "technologies",
   },
+  {
+    type: "input",
+    message: "How can others contribute to your project?\n",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "How do you run tests for your project?\n",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "What's your email address for support inquiries?\n",
+    name: "email",
+  },
+  {
+    type: "list", // Ask if the user wants to include a license
+    message: "Choose a license for your project (or leave blank for none):\n",
+    choices: ["MIT", "GPLv3", "Apache 2.0", "None"],
+    name: "license",
+  },
 ];
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write README file with error handling
 function writeToFile(fileName, data) {
-  fs.writeFileSync(fileName, generateMarkdown(data));
+  try {
+    fs.writeFileSync(fileName, generateMarkdown(data));
+    console.log("README file generated successfully!");
+  } catch (error) {
+    console.error("Error writing file:", error.message);
+  }
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then((data) => {
-    writeToFile("./new_Readme_file/README.md", data),
-      (err) => (err ? console.error(err) : console.log("Success!"));
-  });
+  // Prompt the user and handle errors if writing fails
+  inquirer
+    .prompt(questions)
+    .then((data) => {
+      writeToFile("./new_Readme_file/README.md", data);
+    })
+    .catch((error) => {
+      console.error("Error during inquirer prompt:", error.message);
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,41 +1,84 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a license badge based on the license passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(data) {
-  return `![GitHub](https://img.shields.io/github/license/${data.username}/${data.repo}?style=for-the-badge)`
+  if (data.license === "None") {
+    return ""; // No badge if no license
+  }
+  return `![GitHub](https://img.shields.io/github/license/${data.username}/${data.repo}?style=for-the-badge)`;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(data) {
-  return `https://github.com/${data.username}/${data.repo}/blob/main/LICENSE`
+  if (data.license === "None") {
+    return ""; // No link if no license
+  }
+  return `https://github.com/${data.username}/${data.repo}/blob/main/LICENSE`;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(data) {
-  return `${renderLicenseBadge(data)}<br>See the [license](${renderLicenseLink(data)}) for more details.`
-    }
+  if (data.license === "None") {
+    return ""; // No section if no license
+  }
+  return `${renderLicenseBadge(data)}<br>See the [license](${renderLicenseLink(
+    data
+  )}) for more details.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-${data.intro}
+
+  ${data.intro}
+
+## Table of Contents
+
+- [Features](#features)
+- [Usage](#usage)
+- [Installation](#installation)
+- [Technologies Used](#technologies-used)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
 
 ## Features
 
 - ${data.features}
 
 ## Usage
+
 ${data.usage}
 
 ## Installation
+
 ${data.installation}
 
 ## Technologies Used
-- ${data.technologies}
+
+- ${data.technologies.join(", ")}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
 
 ## License
- ${renderLicenseSection(data)}
+
+${renderLicenseSection(data)}
+
+## Questions
+For any questions or inquiries, please reach out to me via email at: ${
+    data.email
+  }.  
+You can also find me on GitHub: [${data.username}](https://github.com/${
+    data.username
+  }).
 `;
 }
 
